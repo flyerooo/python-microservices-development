d6a78d8024b3e9e30e0001e24da669415382edb4
wangxiaolong <wangxiaolong@zhuotaiju.com>
2018-11-30 04:45:54 +0800

    custom converter

--- flask_basic.py
+++ flask_basic.py
@@ -1,16 +1,35 @@
 from flask import Flask, jsonify, request
+from werkzeug.routing import BaseConverter, ValidationError
+
+_USERS = {'1': 'Tarek', '2': 'Freya'}
+_IDS = {val: id for id, val in _USERS.items()}
+
+
+class RegisteredUser(BaseConverter):
+    def to_python(self, value):
+        if value in _USERS:
+            return _USERS[value]
+        raise ValidationError()
+
+    def to_url(self, value):
+        return _IDS[value]
+
 
 app = Flask(__name__)
+app.url_map.converters['registered'] = RegisteredUser
 
 
-@app.route('/api')
-def my_microservice():
-    print(request)
-    print(request.environ)
-    response = jsonify({'Hello': 'World!'})
-    print(response)
-    print(response.data)
+@app.route('/api/person/<registered:name>')
+def person(name):
+    response = jsonify({'Hello hey': name})
     return response
+
+
+# # @app.route('/api/person/<person_id>')
+# @app.route('/api/person/<int:person_id>')  # 类型转换
+# def person(person_id):
+#     response = jsonify({'Hello': person_id})
+#     return response
 
 
 if __name__ == '__main__':
